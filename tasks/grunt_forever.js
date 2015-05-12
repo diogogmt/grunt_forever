f887d33e9f3dac4b32ecc01fae7d535730ea1ae9
Diogo Monteiro <diogo.gmt@gmail.com>
2015-05-12 12:50:56 -0400

    Added option to define the application name to use in the logs.

--- tasks/grunt_forever.js
+++ tasks/grunt_forever.js
@@ -5,23 +5,24 @@ module.exports = function (grunt) {
   'use strict';
 
   var gruntForever = require('./lib/grunt_forever').init(grunt);
-  grunt.registerMultiTask('grunt_forever', 'Runs a forever monitor of your node.js server.', function () {
+  grunt.registerMultiTask('grunt_forever', 'Monitors a node process with forever.', function () {
     var options = this.options();
     var action = options.action || 'none';
     var done = this.async();
 
+    var appName = options.appName || 'application';
     var actions = {
       start:function (callback) {
         gruntForever.list(options, function (data) {
           if (data.length) {
-            grunt.log.writeln('Stratosphere is already running ' + colors.green('[OK]'));
+            grunt.log.writeln(appName + ' is already running ' + colors.green('[OK]'));
             data.forEach(function (item) {
               grunt.log.writeln('pid: ' + colors.green(item.pid));
             });
             return callback();
           }
           gruntForever.start(options, function () {
-            grunt.log.writeln('Stratosphere started' + colors.green('[OK]'));
+            grunt.log.writeln(appName + ' started' + colors.green('[OK]'));
             callback();
           });
         });
@@ -30,14 +31,14 @@ module.exports = function (grunt) {
       stop: function (callback) {
         gruntForever.list(options, function (data) {
           if (!data.length) {
-            grunt.log.writeln('Stratosphere is not running ' + colors.green('[OK]'));
+            grunt.log.writeln(appName + ' is not running ' + colors.green('[OK]'));
             return callback();
           }
           var totalProcs = data.length;
           var completedProcs = 0;
           data.forEach(function (item) {
             (function (process) {
-              grunt.log.writeln('Stopping stratosphere process ' + colors.green(process.pid));
+              grunt.log.writeln('Stopping ' + appName + ' process ' + colors.green(process.pid));
               gruntForever.stop({ process: process.uid }, function (err) {
                 if (err) {
                   grunt.log.warn('Error stopping process ' + process.pid + ' -' + colors.red(err));
@@ -56,7 +57,7 @@ module.exports = function (grunt) {
         actions.stop(function () {
           // When calling actions.start the forever.list is not returning any data and just halting
           gruntForever.start(options, function (data) {
-            grunt.log.writeln('Stratosphere started ' + colors.green('[OK]'));
+            grunt.log.writeln(appName + ' started ' + colors.green('[OK]'));
             callback();
           });
         });
@@ -65,12 +66,12 @@ module.exports = function (grunt) {
       status: function (callback) {
         gruntForever.list(options, function (data) {
           if (data.length) {
-            grunt.log.writeln('Stratosphere active processes');
+            grunt.log.writeln(appName + ' active processes');
             data.forEach(function (proc) {
               grunt.log.writeln('pid (' + colors.green(proc.pid) + ')');
             });
           } else {
-            grunt.log.writeln('Stratosphere has ' + colors.red(0) + ' active processes');
+            grunt.log.writeln(appName + ' has ' + colors.red(0) + ' active processes');
           }
           callback();
         });
